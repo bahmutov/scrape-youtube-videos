e6aef239fdfdcabe8efc55e47fcd344f7d5bf4c9
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2021-09-23 21:50:34 -0400

    working download of videos

---
+++ README.md
@@ -0,0 +1,7 @@
+# scrape-youtube-videos
+
+- [Google API Node client](https://github.com/googleapis/google-api-nodejs-client)
+
+```shell
+$ as-a . node ./get-videos
+```
---
+++ get-videos.js
@@ -0,0 +1,55 @@
+// const { google } = require('googleapis')
+
+// list all YouTube videos from the given playlist
+// https://developers.google.com/youtube/v3/docs/playlistItems/list
+
+if (!process.env.GOOGLE_API_KEY) {
+  console.error('GOOGLE_API_KEY not set')
+  process.exit(1)
+}
+
+const playlistId = 'PLP9o9QNnQuAYYRpJzDNWpeuOVTwxmIxcI'
+const g = require('@googleapis/youtube')
+const youtube = g.youtube({
+  version: 'v3',
+  auth: process.env.GOOGLE_API_KEY,
+})
+
+async function listVideos() {
+  const videos = []
+  let pageToken
+
+  while (true) {
+    const response = await youtube.playlistItems.list({
+      playlistId,
+      part: 'snippet',
+      pageToken,
+      maxResults: 50,
+    })
+    console.log('found %d video(s)', response.data.items.length)
+    if (!response.data.items.length) {
+      break
+    }
+
+    videos.push(...response.data.items)
+    if (response.data.nextPageToken) {
+      pageToken = response.data.nextPageToken
+    } else {
+      break
+    }
+  }
+
+  console.log('total have %d video(s)', videos.length)
+  const list = videos.map((video) => {
+    return {
+      videoId: video.snippet.resourceId.videoId,
+      title: video.snippet.title,
+      description: video.snippet.description,
+      publishedAt: video.snippet.publishedAt,
+    }
+  })
+
+  return list
+}
+
+listVideos().then(console.log, console.error)
